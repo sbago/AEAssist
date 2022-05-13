16c472820fdedf748bde9cb120d3d0f27b387fa4
KaiserBh <kaiserbh@protonmail.com>
2022-05-14 05:00:07 +1000

    added check if reborn is running

--- Scripts/PostBuild.bat
+++ Scripts/PostBuild.bat
@@ -1,5 +1,15 @@
+@echo off
 Rem Kill RebornBuddy if it is running Make sure to run the bat file as administrator or it will not work. (Run Rider, or Visual Studio as administrator)
-taskkill /F /IM "RebornBuddy.exe"
+Rem check if RebornBuddy.exe is running
+SETLOCAL EnableExtensions
+SET EXE=RebornBuddy.exe
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF NOT %%x == %EXE% (
+  ECHO %EXE% is Not Running
+) ELSE (
+  ECHO %EXE is running
+  taskkill /F /IM "RebornBuddy.exe"
+)
+
 Rem Get current directory + Output folder 
 set currentdir=%cd%
 Rem Make sure change this to your own directory
@@ -9,4 +19,5 @@ set currentdir=%currentdir%\..\Output
 cd %currentdir%
 Rem Copy everything from the output folder to the specified folder
 xcopy /s/z /y %currentdir% %outputdir%
+pause
 
