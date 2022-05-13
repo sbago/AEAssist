6b7b41098c0954e8bd5a027c5b3d3c3110c204c2
KaiserBh <KaiserBh@protonmail.com>
2022-05-13 16:41:54 +1000

    Added potion usage for sage.

--- AEAssist/AI/Sage/Ability/SageAbilityUsePotion.cs
+++ AEAssist/AI/Sage/Ability/SageAbilityUsePotion.cs
@@ -1,7 +1,34 @@
-﻿namespace AEAssist.AI.Sage.Ability
+﻿using System.Threading.Tasks;
+using AEAssist.Define;
+using AEAssist.Helper;
+using ff14bot;
+using ff14bot.Objects;
+
+namespace AEAssist.AI.Sage.Ability
 {
-    public class SageAbilityUsePotion
+    public class SageAbilityUsePotion : IAIHandler
     {
-        
+        public int Check(SpellEntity lastSpell)
+        {
+            if (!SettingMgr.GetSetting<GeneralSettings>().UsePotion)
+                return -1;
+            if (AIRoot.Instance.CloseBurst)
+                return -2;
+            if (TTKHelper.IsTargetTTK(Core.Me.CurrentTarget as Character))
+                return -4;
+            if (!PotionHelper.CheckPotion(SettingMgr.GetSetting<GeneralSettings>().MindPotionId))
+                return -6;
+            
+            return 0;
+        }
+
+        public async Task<SpellEntity> Run()
+        {
+            var ret = await PotionHelper.UsePotion(SettingMgr.GetSetting<GeneralSettings>().MindPotionId);
+            if (ret) AIRoot.Instance.MuteAbilityTime();
+
+            await Task.CompletedTask;
+            return null;
+        }
     }
 }
