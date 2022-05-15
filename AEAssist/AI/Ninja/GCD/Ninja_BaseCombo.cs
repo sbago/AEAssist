60515d74db7bc4754d45c533320d2eafac1060b5
AnotherEnd <210379417@qq.com>
2022-05-15 23:09:50 +0800

    ninja init commit

---
+++ AEAssist/AI/Ninja/GCD/Ninja_BaseCombo.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using AEAssist.Define;
+using AEAssist.Helper;
+
+namespace AEAssist.AI.Ninja.GCD
+{
+    public class Ninja_BaseCombo : IAIHandler
+    {
+        public int Check(SpellEntity lastSpell)
+        {
+            return 0;
+        }
+
+        public async Task<SpellEntity> Run()
+        {
+            var spell = SpellsDefine.SpinningEdge.GetSpellEntity();
+            if (await spell.DoGCD())
+                return spell;
+            return null;
+        }
+    }
+}
---
+++ AEAssist/AI/Ninja/Ninja_AIPriority.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AEAssist.AI.Ninja.GCD;
+using AEAssist.Helper;
+using ff14bot.Enums;
+
+namespace AEAssist.AI.Ninja
+{
+    [Job(ClassJobType.Ninja)]
+    public class Ninja_AIPriority : IAIPriorityQueue
+    {
+        public List<IAIHandler> GCDQueue { get; } = new List<IAIHandler>()
+        {
+            new Ninja_BaseCombo(),
+        };
+
+        public List<IAIHandler> AbilityQueue { get; } = new List<IAIHandler>()
+        {
+
+        };
+        public async Task<bool> UsePotion()
+        {
+            return await PotionHelper.ForceUsePotion(SettingMgr.GetSetting<GeneralSettings>().DexPotionId);
+        }
+    }
+}
