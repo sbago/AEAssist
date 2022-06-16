ce3c7f6e068842929baab06adcd307aadadce604
AnotherEnd <210379417@qq.com>
2022-06-16 22:19:18 +0800

    add targetType

--- AETriggers/TriggerModel/TriggerAction/TriggerAction_CastGCD.cs
+++ AETriggers/TriggerModel/TriggerAction/TriggerAction_CastGCD.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AEAssist.View;
 using PropertyChanged;
 
 namespace AEAssist.TriggerAction
@@ -8,7 +9,9 @@ namespace AEAssist.TriggerAction
     public class TriggerAction_CastGCD : ITriggerAction
     {
         public uint SpellId{ get; set; }
-
+        [GUIIntRange(0,3)]
+        [GUIToolTip("0: DefaultByCode 1: Self 2: Target 3: Target's Target \n 0: 默认 1: 自己 2: 目标 3: 目标的目标 ")]
+        public uint TargetType { get; set; }
         public void WriteFromJson(string[] values)
         {
             if (!uint.TryParse(values[0], out var spell)) throw new Exception($"{values[0]}Error!");
