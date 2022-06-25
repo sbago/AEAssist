65f872896db2e5f090fbc8fbc9c6d4c2c41464ef
KaiserBh <kaiserbh@protonmail.com>
2022-06-26 04:57:49 +1000

    added debug log, maybe remove it when you are done.

--- AEAssist/DataBinding/MeleePosition.cs
+++ AEAssist/DataBinding/MeleePosition.cs
@@ -74,6 +74,7 @@ namespace AEAssist
 
         public Priority GetPriority()
         {
+            LogHelper.Debug("Current Priority is: " + CurrentPriority);
             return CurrentPriority;
         }
 
