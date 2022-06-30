9d6f2ada36a554f62954fa3b7a3976338c8e6ba9
KaiserBh <kaiserbh@protonmail.com>
2022-06-30 14:44:39 +1000

    added "fillers" for samurai.

--- AEAssist/AI/Samurai/GCD/SamuraiGCD_Fillers.cs
+++ AEAssist/AI/Samurai/GCD/SamuraiGCD_Fillers.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using AEAssist.Define;
+using AEAssist.Helper;
+using AEAssist.Rotations.Core;
 
 namespace AEAssist.AI.Samurai.GCD
 {
@@ -7,12 +10,82 @@ namespace AEAssist.AI.Samurai.GCD
     {
         public int Check(SpellEntity lastSpell)
         {
-            throw new System.NotImplementedException();
+            const int minimumGcdOneMin = 26;
+            var baseGcdTime = RotationManager.Instance.GetBaseGCDSpell().AdjustedCooldown.TotalMilliseconds;
+            var battleTime = AIRoot.GetBattleData<BattleData>().CurrBattleTimeInMs;
+            var totalGcdTimeMs = baseGcdTime * minimumGcdOneMin;
+
+            if (battleTime % totalGcdTimeMs == 0)
+            {
+                return 0;
+            }
+
+            return -1;
         }
 
-        public Task<SpellEntity> Run()
+        public async Task<SpellEntity> Run()
         {
-            throw new System.NotImplementedException();
+            var baseGcdTime = RotationManager.Instance.GetBaseGCDSpell().AdjustedCooldown.TotalMilliseconds;
+
+            if (Math.Abs(baseGcdTime - 2140) == 0) // 2.14second gcd 2 filler gcd needed
+            {
+                if (await SpellsDefine.Hakaze.DoGCD())
+                {
+                    return SpellsDefine.Hakaze.GetSpellEntity();
+                }
+
+                if (await SpellsDefine.Yukikaze.DoGCD())
+                {
+                    return SpellsDefine.Yukikaze.GetSpellEntity();
+                }
+
+                if (await SpellsDefine.Hagakure.DoAbility())
+                {
+                    return SpellsDefine.Hagakure.GetSpellEntity();
+                }
+            } else if (Math.Abs(baseGcdTime - 2070) == 0) // 2.07sec gcd 3 filler gcd needed
+            {
+                if (await SpellsDefine.Hakaze.DoGCD())
+                {
+                    return SpellsDefine.Hakaze.GetSpellEntity();
+                }
+                
+                if (await SpellsDefine.Jinpu.DoGCD())
+                {
+                    return SpellsDefine.Jinpu.GetSpellEntity();
+                }
+                
+                if (await SpellsDefine.Gekko.DoGCD())
+                {
+                    return SpellsDefine.Gekko.GetSpellEntity();
+                }
+                
+                if (await SpellsDefine.Hagakure.DoAbility())
+                {
+                    return SpellsDefine.Hagakure.GetSpellEntity();
+                }
+            }else if (Math.Abs(baseGcdTime - 2000) == 0) // 2.00 seconds gcd 4 filler gcd needed (get better gear lol)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    if (await SpellsDefine.Hakaze.DoGCD())
+                    {
+                        return SpellsDefine.Hakaze.GetSpellEntity();
+                    }
+
+                    if (await SpellsDefine.Yukikaze.DoGCD())
+                    {
+                        return SpellsDefine.Yukikaze.GetSpellEntity();
+                    }
+                
+                    if (await SpellsDefine.Hagakure.DoAbility())
+                    {
+                        return SpellsDefine.Hagakure.GetSpellEntity();
+                    }       
+                }
+            }
+
+            return null;
         }
     }
 }
