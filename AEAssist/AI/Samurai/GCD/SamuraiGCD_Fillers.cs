2036d3fa39fda44b6112594c6fc5efde08ac8df2
KaiserBh <kaiserbh@protonmail.com>
2022-06-30 11:47:14 +1000

    TODO: fix the filler gcds and fix the opener as well add directional hint.

---
+++ AEAssist/AI/Samurai/GCD/SamuraiGCD_Fillers.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using AEAssist.Define;
+
+namespace AEAssist.AI.Samurai.GCD
+{
+    public class SamuraiGCD_Fillers : IAIHandler
+    {
+        public int Check(SpellEntity lastSpell)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<SpellEntity> Run()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
